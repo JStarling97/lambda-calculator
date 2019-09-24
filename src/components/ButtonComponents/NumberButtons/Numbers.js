d69ef7eda230b34acadd75628125d48eeb4980d0
Joshua Starling <joshstarling97@gmail.com>
2019-09-24 12:52:50 -0400

    keep forgetting to commit, but finished

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import NumberButton from './NumberButton';
-import { numbers } from './data';
-import { tsPropertySignature } from "@babel/types";
-
+import NumberButton from "./NumberButton";
+import { numbers } from "../../../data";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
@@ -10,24 +8,27 @@ import { tsPropertySignature } from "@babel/types";
 import { numbers } from '../../../data' 
 */
 //Import your array data to from the provided data file
-
-const Numbers = () => {
+const Numbers = (props) => {
   // STEP 2 - add the imported data to state
+  // const [numsData, setNumsData] = useState('');
+  // console.log(props);
   return (
-    <div className='num-container'>
+    <div className="num-container">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
       {
         numbers.map((num, index) => {
-          if (num === '.') {
+          if (num === ".") {
             return <NumberButton key={index} number={num} handleNumClick={props.handleClick} state={useState} />
           } else
-            return <NumberButton key={index} number={parseInt(num)} handleNumClick={props.handleNumClick} state={useState} />
+            return <NumberButton key={index} number={parseInt(num)} handleNumClick={props.handleClick} state={useState} />
         })
       }
+
     </div>
   );
+
 };
 
 export default Numbers;
--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,11 @@
 import React from "react";
+// import NumberButton from "../ButtonComponents/NumberButtons/NumberButton";
+// import OperatorButton from "../ButtonComponents/OperatorButtons/OperatorButton";
+// import SpecialButton from "../ButtonComponents/SpecialButtons/SpecialButton";
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+
+const Display = (props) => {
+  return (<div className="display">{`${props.dispNum}`}</div>);
 };
+
+export default Display;
